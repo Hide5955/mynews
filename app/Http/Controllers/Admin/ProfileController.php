9a8451a3758b50b7a631ffc467c08855173b26d3
Hide <hideyuki.kawasaki@rakuten-sec.co.jp>
2021-09-23 21:49:20 +0900

    12章

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -2,29 +2,64 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+use App\Profile;
+
 
 class ProfileController extends Controller
 {
-    //以下、課題で追記の内容
-    public function add()
-{
-    return view('admin.profile.create');
-}
+  public function add()
+  {
+      return view('admin.profile.create');
+  }
 
-public function create()
-{
-    return redirect('admin/profile/create');
-}
+  public function create(Request $request)
+  {
 
-public function edit()
-{
-    return view('admin.profile.edit');
-}
+      // Varidationをおこなう
+      $this->validate($request, Profile::$rules);
 
-public function update()
-{
-    return redirect('admin/profile/edit');
-}
+      $profile = new Profile;
+      $form = $request->all();
+
+      // formに画像があれば、保存する
+    
+      unset($form['_token']);
+     
+      // データベースに保存する
+      $profile->fill($form);
+      $profile->save();
+
+      return redirect('admin/profile/create');
+  }
+
+
+  public function edit(Request $request)
+  {
+      // Profile Modelからデータを取得する
+      $profile = Profile::find($request->id);
+      if (empty($profile)) {
+        abort(404);    
+      }
+      return view('admin.profile.edit', ['profile_form' => $profile]);
+  }
+
+  public function update(Request $request)
+  {
+      // Validationをかける
+      $this->validate($request, Profile::$rules);
+      // Profile Modelからデータを取得する
+      $profile = Profile::find($request->id);
+      // 送信されてきたフォームデータを格納する
+      $profile_form = $request->all();
+
+      unset($profile_form['remove']);
+
+      // 該当するデータを上書きして保存する
+      $profile->fill($profile_form)->save();
+
+      return redirect('admin/profile');
+  }
 }
